ae0654db917f995d0bdd8fdab0db37c1cda8ff9c
ievertan00 <ievertan00@gmail.com>
2025-09-16 16:59:41 +0800

    fix: Set withCredentials to true in axios config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://bsm-project.onrender.com',
+  withCredentials: true
+});
+
+api.interceptors.request.use(request => {
+  console.log('Starting Request', request);
+  return request;
 });
 
 export default api;
